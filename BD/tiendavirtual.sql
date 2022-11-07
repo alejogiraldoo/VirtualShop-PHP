999c5d154a5852601fb0268f75e1bace1b5371f6
Alejandro Giraldo Ocampo <alejandrogiraldo1404@gmail.com>
2022-11-07 13:29:00 -0500

    Agregando codigo de la base de datos y el esquema del modelo relacional

---
+++ BD/tiendavirtual.sql
@@ -0,0 +1,266 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 07-11-2022 a las 17:28:31
+-- Versión del servidor: 5.7.33
+-- Versión de PHP: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tiendavirtual`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asesor`
+--
+
+CREATE TABLE `asesor` (
+  `numAs` int(11) NOT NULL,
+  `id` varchar(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `telefono` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallesventa`
+--
+
+CREATE TABLE `detallesventa` (
+  `numdV` int(11) NOT NULL,
+  `numPro` int(11) NOT NULL,
+  `numV` int(11) NOT NULL,
+  `numMetP` int(11) NOT NULL,
+  `cantidad` bigint(20) NOT NULL,
+  `estado` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `metodopago`
+--
+
+CREATE TABLE `metodopago` (
+  `numMetP` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `descripcion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `numPro` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `precio` bigint(20) NOT NULL,
+  `cantidad` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `numRol` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rolesusuario`
+--
+
+CREATE TABLE `rolesusuario` (
+  `numUsu` int(11) NOT NULL,
+  `numRol` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `numUsu` int(11) NOT NULL,
+  `id` varchar(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `telefono` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `numV` int(11) NOT NULL,
+  `numUsu` int(11) NOT NULL,
+  `numAs` int(11) DEFAULT NULL,
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `asesor`
+--
+ALTER TABLE `asesor`
+  ADD PRIMARY KEY (`numAs`);
+
+--
+-- Indices de la tabla `detallesventa`
+--
+ALTER TABLE `detallesventa`
+  ADD PRIMARY KEY (`numdV`),
+  ADD KEY `numV` (`numV`),
+  ADD KEY `numPro` (`numPro`),
+  ADD KEY `numMetP` (`numMetP`);
+
+--
+-- Indices de la tabla `metodopago`
+--
+ALTER TABLE `metodopago`
+  ADD PRIMARY KEY (`numMetP`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`numPro`),
+  ADD KEY `nombreProducto` (`nombre`),
+  ADD KEY `precioProducto` (`precio`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`numRol`),
+  ADD UNIQUE KEY `nombreRol` (`nombre`);
+
+--
+-- Indices de la tabla `rolesusuario`
+--
+ALTER TABLE `rolesusuario`
+  ADD KEY `numUsu` (`numUsu`),
+  ADD KEY `numRol` (`numRol`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`numUsu`),
+  ADD UNIQUE KEY `idUsuario` (`id`),
+  ADD KEY `nombreUsuario` (`nombre`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`numV`),
+  ADD KEY `numAs` (`numAs`),
+  ADD KEY `numUsu` (`numUsu`),
+  ADD KEY `fechaVenta` (`fecha`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asesor`
+--
+ALTER TABLE `asesor`
+  MODIFY `numAs` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detallesventa`
+--
+ALTER TABLE `detallesventa`
+  MODIFY `numdV` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `metodopago`
+--
+ALTER TABLE `metodopago`
+  MODIFY `numMetP` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `numPro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `numRol` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `numUsu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `numV` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detallesventa`
+--
+ALTER TABLE `detallesventa`
+  ADD CONSTRAINT `detallesventa_ibfk_1` FOREIGN KEY (`numV`) REFERENCES `venta` (`numV`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detallesventa_ibfk_2` FOREIGN KEY (`numPro`) REFERENCES `producto` (`numPro`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detallesventa_ibfk_3` FOREIGN KEY (`numMetP`) REFERENCES `metodopago` (`numMetP`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `rolesusuario`
+--
+ALTER TABLE `rolesusuario`
+  ADD CONSTRAINT `rolesusuario_ibfk_1` FOREIGN KEY (`numUsu`) REFERENCES `usuarios` (`numUsu`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rolesusuario_ibfk_2` FOREIGN KEY (`numRol`) REFERENCES `rol` (`numRol`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`numAs`) REFERENCES `asesor` (`numAs`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`numUsu`) REFERENCES `usuarios` (`numUsu`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
